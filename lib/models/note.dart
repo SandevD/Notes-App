56ed80b1a17a5d50377184813024c872845ef9b6
sandevD <sandev.net@gmail.com>
2024-11-03 08:37:31 +0530

    ✨ Add title field to Note model

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,10 +1,11 @@
 class Note {
   final int status, id;
-  final String content;
+  final String title, content;
 
   Note({
     required this.id,
     required this.status,
+    required this.title,
     required this.content,
   });
 }
