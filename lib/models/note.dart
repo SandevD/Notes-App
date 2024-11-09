436e0bdb2abc094b8c431edf9eb1ba664edf12fd
sandevD <sandev.net@gmail.com>
2024-11-10 00:58:51 +0530

    🎨 Add createdAt and updatedAt fields to Note model for better tracking

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,11 +1,13 @@
 class Note {
   final int status, id;
-  final String title, content;
+  final String title, content, createdAt, updatedAt;
 
   Note({
     required this.id,
     required this.status,
     required this.title,
     required this.content,
+    required this.createdAt,
+    required this.updatedAt,
   });
 }
